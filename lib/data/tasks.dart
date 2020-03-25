3801f5992a5f149e282e67a76805094425782d95
srikii <44855671+srikii@users.noreply.github.com>
2020-03-25 18:01:19 -0400

    queries for today page and planned page incomplete

--- lib/data/tasks.dart
+++ lib/data/tasks.dart
@@ -168,6 +168,121 @@ class Showstar extends StatelessWidget {
 
 }
 
+
+class CompletedTask extends StatelessWidget {
+
+  final String uid;
+  const CompletedTask({Key key, this.uid}) :super(key: key);
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: Firestore.instance.collection('tasks').where(
+          'uid', isEqualTo: "$uid").where('iscompleted', isEqualTo: true).orderBy(
+          'iscompleted', descending: false).snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) return Container();
+        return _buildList2(context, snapshot.data.documents);
+      },
+    );
+  }
+
+  Widget _buildList2(BuildContext context, List<DocumentSnapshot> list) {
+    return ListView.builder(
+        itemCount: list.length,
+        itemBuilder: (context, idx) {
+          DocumentSnapshot doc = list[idx];
+
+          return ListTile(
+
+            leading:
+            IconButton(
+              icon: changeicon_com(doc['iscompleted']),
+              onPressed: () => completed(doc),
+
+            ),
+
+            trailing:
+            Icon(Icons.check),
+
+            title: Text(doc['name'], style: returnstyle(doc['iscompleted']),),
+            subtitle: Text(
+                doc['comment'], style: returnstyle(doc['iscompleted'])),
+          );
+        }
+    );
+  }
+  Future completed(DocumentSnapshot document) {
+    if (document['iscompleted']) {
+      document.reference.updateData({"iscompleted": false});
+    }
+    else {
+      document.reference.updateData({"iscompleted": true});
+    }
+  }
+
+  returnstyle(bool completed) {
+    if (completed) {
+      return TextStyle(fontWeight: FontWeight.w200);
+    }
+    else {
+      return TextStyle(fontWeight: FontWeight.normal);
+    }
+
+  }
+
+
+  changeicon_com(bool completed) {
+    return completed ? Icon(Icons.check_box) : Icon(
+        Icons.check_box_outline_blank);
+  }
+
+}
+
+//queries for today page and planned page incomplete
+/*
+class TodayTask extends StatelessWidget {
+
+  final String uid;
+  const TodayTask({Key key, this.uid}) :super(key: key);
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: Firestore.instance.collection('tasks').where(
+          'uid', isEqualTo: "$uid").where('time', isEqualTo: DateTime.now()).snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) return Container();
+        return _buildList3(context, snapshot.data.documents);
+      },
+    );
+  }
+}
+
+
+class ShowPlanned extends StatelessWidget {
+
+  final String uid;
+  const ShowPlanned({Key key, this.uid}) :super(key: key);
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: Firestore.instance.collection('tasks').where(
+          'uid', isEqualTo: "$uid").where('time', isEqualTo: DateTime.now()).snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) return Container();
+        return _buildList3(context, snapshot.data.documents);
+      },
+    );
+  }
+}
+*/
+
+
 class Tasks {
   final String name;
   final String uid;
