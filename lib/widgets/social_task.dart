72c1e9c9daf007e812725e0d5ac69e074db9d33e
Yuan <byrd.zhang@gmail.com>
2020-04-25 17:03:20 -0400

    Update Done UI for Friends Dashboard

--- lib/widgets/social_task.dart
+++ lib/widgets/social_task.dart
@@ -69,91 +69,108 @@ class _SocialTaskState extends State<SocialTask> {
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(10.0),
               ),
-              child: Column(
+              child: Stack(
                 children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 10.0),
-                    child: ListTile(
-                      leading: CircleAvatar(
-                        backgroundImage: CachedNetworkImageProvider(
-                            widget.task.user['photoURL']),
-                        backgroundColor: Colors.grey,
-                        radius: 18,
-                      ),
-                      contentPadding: EdgeInsets.all(2),
-                      title: Text(
-                        "${widget.task.user['displayName']}",
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.cyan[900],
+                  Positioned(
+                    top: 10,
+                    left:150,
+                    child: Opacity(
+                      opacity:  widget.task.iscompleted ? 0.5 : 0.0,
+                      child: Image.asset(
+                        'assets/images/Done_postmark.png',
+                        width: 110,
+                      )
+                  ),),
+
+
+                  Column(
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 10.0),
+                        child: ListTile(
+                          leading: CircleAvatar(
+                            backgroundImage: CachedNetworkImageProvider(
+                                widget.task.user['photoURL']),
+                            backgroundColor: Colors.grey,
+                            radius: 18,
+                          ),
+                          contentPadding: EdgeInsets.all(2),
+                          title: Text(
+                            "${widget.task.user['displayName']}",
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.cyan[900],
+                            ),
+                          ),
+                          onTap: () => Navigator.pushNamed(context, '/myprofile',
+                              arguments: widget.task.user['uid']),
+                          trailing: Text(
+                            "${timeago.format(widget.task.create.toDate())}",
+                            style: TextStyle(
+                              fontSize: 12,
+                              color: Colors.grey[600],
+                            ),
+                          ),
                         ),
                       ),
-                      onTap: () => Navigator.pushNamed(context, '/myprofile',
-                          arguments: widget.task.user['uid']),
-                      trailing: Text(
-                          "${timeago.format(widget.task.create.toDate())}",
-                          style: TextStyle(
-                            fontSize: 12,
-                            color: Colors.grey[600],
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 10.0),
+                        child: ListTile(
+                          contentPadding: EdgeInsets.all(2),
+                          //leading: widget.task.iscompleted == true
+                          //? Icon(Icons.check_box)
+                          //: Icon(Icons.check_box_outline_blank),
+                          title: Text(
+                            "${widget.task.name}",
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.black,
+                            ),
                           ),
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 10.0),
-                    child: ListTile(
-                      contentPadding: EdgeInsets.all(2),
-                      leading: widget.task.iscompleted == true
-                          ? Icon(Icons.check_box)
-                          : Icon(Icons.check_box_outline_blank),
-                      title: Text(
-                        "${widget.task.name}",
-                        style: TextStyle(
-                          fontSize: 16,
-                          color: Colors.black,
-                        ),
-                      ),
-                      subtitle: Text(
-                          "${widget.task.comment}",
-                          style: TextStyle(
-                            fontSize: 12,
-                            color: Colors.grey[600],
+                          subtitle: Text(
+                            "${widget.task.comment}",
+                            style: TextStyle(
+                              fontSize: 12,
+                              color: Colors.grey[600],
+                            ),
                           ),
-                      ),
-                      onTap: () => Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => TaskDetails(
+                          onTap: () => Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => TaskDetails(
                                     task: widget.task,
                                     uid: widget.uid,
                                   ))),
-                      trailing: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: <Widget>[
-                          Text("${this.likeCount}"),
-                          IconButton(
-                            icon: Icon(Icons.thumb_up),
-                            color: isLiked == true ? Colors.cyan[700] : Colors.grey[500],
-                            onPressed: isLiked == true ? unlike : handleLike,
-                          ),
-                          IconButton(
-                            icon: Icon(Icons.comment),
-                            color: Colors.grey[500],
-                            onPressed: () => Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => CommentsPage(
+                          trailing: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: <Widget>[
+                              Text("${this.likeCount}"),
+                              IconButton(
+                                icon: Icon(Icons.thumb_up),
+                                color: isLiked == true ? Colors.cyan[700] : Colors.grey[500],
+                                onPressed: isLiked == true ? unlike : handleLike,
+                              ),
+                              IconButton(
+                                icon: Icon(Icons.comment),
+                                color: Colors.grey[500],
+                                onPressed: () => Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => CommentsPage(
                                           task: widget.task,
                                           uid: widget.uid,
                                         ))),
+                              ),
+                            ],
                           ),
-                        ],
+                        ),
                       ),
-                    ),
+                    ],
                   ),
                 ],
-              ),
+              )
+
             ),
             Divider(color: Color(0xEBEBEB)),
           ],
